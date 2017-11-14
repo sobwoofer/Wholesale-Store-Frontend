e29e40e7b4dabec3e4bd1be2d9c30e6127a584bb
volynets <volynets@papirus.com.ua>
2017-11-14 15:26:09 +0200

    added package json to npm and configure gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,49 @@
 
 var gulp = require('gulp');
+    sass = require('gulp-sass'); // for compilate scss to css
+    rigger = require('gulp-rigger'); //rigger for including files in parent file
 
-var sass = require('gulp-sass');
+var path = {
+    build: { //These finished after building files
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: { //Paths from getting sources
+        html: 'src/*.html',
+        js: 'src/js/main.js',
+        style: 'src/style/main.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { //What kind of files and paths we want to watching.
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/style/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = { //config of our dev server
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Devil"
+};
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
+});
 
 gulp.task('sass', function () {
     return gulp.src('./sass/**/*.scss')
@@ -13,11 +55,13 @@ gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-var rigger = require('gulp-rigger');
+
 
 gulp.task('default', function () {
     gulp.src('app/*.js')
         .pipe(rigger())
         .pipe(gulp.dest('build/'));
 });
+
+
 
