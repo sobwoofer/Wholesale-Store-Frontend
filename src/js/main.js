f2c3d6eaa8ec867f0f64a6125f258d75c5843542
volynets <volynets@papirus.com.ua>
2017-11-15 14:32:31 +0200

    structured style and add brand section to home page

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+window.currentSlide = "brand_i_1";
+
+
+$(function () {
+
+    $('.brands-sl-item').click(function() {
+        var t = $(this).data("id");
+        if (window.currentSlide != t){
+            window.currentSlide = t;
+            $('.brands-item').slideUp(300);
+            $('.brands-items').find("[data-id='" + t + "']").slideDown(300);
+        }
+    });
+
+    $('.brands-sl').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        infinite: true,
+        centerMode: true,
+        //centerPadding: '0px',
+        variableWidth: true,
+        focusOnSelect: true
+    });
+
+
+
+});
+
+function createSlick() {
+
+}
