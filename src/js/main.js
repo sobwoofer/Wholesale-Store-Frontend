5e01a5ddb9cf610e260f251733b873dbc2b14090
volynets <volynets@papirus.com.ua>
2017-11-16 16:30:38 +0200

    added auto anchors to menu

--- src/js/main.js
+++ src/js/main.js
@@ -25,8 +25,46 @@ $(function () {
 
 
 
+
 });
 
-function createSlick() {
 
+//anchor script
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top-100
+        }, 500, 'swing', function () {
+            // window.location.hash = $(target).offset().top+120;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop()+200;
+    $('.navbar-nav a[href^="#"]').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement && refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.navbar-nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        } else {
+            currLink.removeClass("active");
+        }
+    });
 }
