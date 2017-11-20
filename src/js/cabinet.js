2254612cde785b280e14524343f5d1e4ec5402d3
volynets <volynets@papirus.com.ua>
2017-11-20 17:11:52 +0200

    added catalog page

--- src/js/cabinet.js
+++ src/js/cabinet.js
@@ -75,6 +75,31 @@ $(function () {
     $("#DateCountdown").TimeCircles();
 });
 
+//filter
+$(function () {
+    $('.filter_cat_wrp .toggle-btn').click(function (e) {
+        $($(this).parent().parent('.filter_cat_wrp')).toggleClass('active')
+    });
+    $('#showFilter').click(function (e) {
+        $('.catalog-filter').slideToggle()
+    });
+    $("#slider-range").slider({
+        min: 0,
+        max: 1000,
+        values: [0, 1000],
+        range: true,
+        stop: function (event, ui) {
+            $("input#minCost").val($("#slider-range").slider("values", 0));
+            $("input#maxCost").val($("#slider-range").slider("values", 1));
+        },
+        slide: function (event, ui) {
+            $("input#minCost").val($("#slider-range").slider("values", 0));
+            $("input#maxCost").val($("#slider-range").slider("values", 1));
+        }
+    });
+});
+
+
 
 //menu button
 $(function () {
