6936c1666bdf2e81da80d71477faa709b2f2e35e
volynets <volynets@papirus.com.ua>
2017-12-04 12:40:00 +0200

    move script categories auto height

--- src/js/cabinet.js
+++ src/js/cabinet.js
@@ -44,36 +44,10 @@ $(function () {
 
 //= source/mobile-catalog.js
 
-//categories border with auto row height
-$(function(){
-    if ($(window).width() > 991) {
-        sortItemsTo(4, $('.categories .item'));
-    } else if($(window).width() > 768 && $(window).width() <= 991) {
-        sortItemsTo(3, $('.categories .item'));
-    } else if($(window).width() > 575 && $(window).width() <= 768) {
-        sortItemsTo(2, $('.categories .item'));
-    }
+//Demo show Cart Error Popup
+function ShowCartErrorPopup() {
+    $('#cartErrorPopup').modal('show');
+}
 
-    function sortItemsTo(num, jQueryElements) {
-        var sizes = [];
-        var count = [];
-        var maxSize = 0;
-        var remainder = jQueryElements.length%num;
-        jQueryElements.each(function(i, e){
-            sizes.push(parseInt($(e).height()));
-            count.push(parseInt(i));
-            if (jQueryElements.length - i <= remainder) {
-                num = remainder;
-            }
-            if (i == 0 || (i+1)%num == 0) {
-                maxSize = Math.max.apply(null, sizes);
-                count.forEach(function (e, i){
-                    $('.categories .item:nth-child(' + (e + 1) + ')').height(maxSize);
-                });
-                sizes = [];
-                count = [];
-                maxSize = 0
-            }
-        });
-    }
-});
+//categories border with auto row height
+//= source/categories-auto-height.js
---
+++ src/js/source/categories-auto-height.js
@@ -0,0 +1,32 @@
+$(function(){
+    if ($(window).width() > 991) {
+        sortItemsTo(4, $('.categories .item'));
+    } else if($(window).width() > 768 && $(window).width() <= 991) {
+        sortItemsTo(3, $('.categories .item'));
+    } else if($(window).width() > 575 && $(window).width() <= 768) {
+        sortItemsTo(2, $('.categories .item'));
+    }
+
+    function sortItemsTo(num, jQueryElements) {
+        var sizes = [];
+        var count = [];
+        var maxSize = 0;
+        var remainder = jQueryElements.length%num;
+        jQueryElements.each(function(i, e){
+            sizes.push(parseInt($(e).height()));
+            count.push(parseInt(i));
+            if (jQueryElements.length - i <= remainder) {
+                num = remainder;
+            }
+            if (i == 0 || (i+1)%num == 0) {
+                maxSize = Math.max.apply(null, sizes);
+                count.forEach(function (e, i){
+                    $('.categories .item:nth-child(' + (e + 1) + ')').height(maxSize);
+                });
+                sizes = [];
+                count = [];
+                maxSize = 0
+            }
+        });
+    }
+});
