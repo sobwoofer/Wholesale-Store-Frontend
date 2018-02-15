99218faa1088910a2d386a2605e903dd3f7b026e
volynets <volynets@papirus.com.ua>
2018-02-15 13:37:03 +0200

    integrated masonry layout library for left menu

--- src/js/source/top-catalog.js
+++ src/js/source/top-catalog.js
@@ -13,10 +13,39 @@ $(function () {
         setTimeout(function () {
             if ($(thisItem).is(":hover") || $(thisItem).hasClass('active')) {
                 $(thisItem).toggleClass('active');
+
                 if ($(thisItem).hasClass('active')) {
-                    $('.level2', thisItem).show('fast');
+                    $('.level2', thisItem).show(50);
                 } else {
-                    $('.level2', thisItem).hide('fast');
+                    $('.level2', thisItem).hide(50);
+                }
+            }
+        }, 200);
+    });
+    window.menuGrid = false;
+    $('.level2 > li').hover(function () {
+        var thisItem = this;
+        setTimeout(function () {
+            if ($(thisItem).is(":hover") || $(thisItem).hasClass('active')) {
+                $(thisItem).toggleClass('active');
+
+                if ($(thisItem).hasClass('active')) {
+                    $('.sub_menu', thisItem).fadeIn(100);
+
+                    // if (menuGrid){
+                    //     $('.grid').masonry('destroy');
+                    // }
+                    if (window.menuGrid) {
+                        $('.grid').masonry('destroy');
+                    }
+
+                    window.menuGrid = $('.grid').masonry({
+                        columnWidth: 280,
+                        itemSelector: '.item',
+                        isFitWidth: true
+                    });
+                } else {
+                    $('.sub_menu', thisItem).fadeOut(150);
                 }
             }
         }, 200);
